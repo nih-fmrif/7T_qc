362a7882730643f047e7aa9ed66046d80d1cc7dd
Jan Varada <jan.varada@nih.gov>
2016-11-17 14:45:06 -0500

    Added check for output directory emptiness before starting analysis

--- run_analysis.py
+++ run_analysis.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import logging
+import shutil
 from concurrent.futures import ThreadPoolExecutor, wait
 from threading import Semaphore
 from utils import log_output, create_path
@@ -12,6 +13,11 @@ from datetime import datetime
 
 
 MAX_WORKERS = cpu_count() * 5
+
+
+class DuplicateFile(Exception):
+    def __init__(self, message):
+        self.message = message
 
 
 if __name__ == "__main__":
@@ -43,6 +49,13 @@ if __name__ == "__main__":
         default=MAX_WORKERS
     )
 
+    parser.add_argument(
+        "--overwrite",
+        help="Overwrite existing BIDS files. NOT RECOMMENDED.",
+        action="store_true",
+        default=False
+    )
+
     settings = parser.parse_args()
 
     if not os.path.isdir(settings.log_dir):
@@ -68,13 +81,26 @@ if __name__ == "__main__":
     settings_str = "Bids directory: {}\n".format(settings.bids_dir) + \
                    "Output directory: {}\n".format(settings.output_dir) + \
                    "Log directory: {}\n".format(settings.log_dir) + \
-                   "No. of Threads: {}\n".format(settings.nthreads)
+                   "No. of Threads: {}\n".format(settings.nthreads) + \
+                   "Overwrite: {}\n".format(settings.overwrite)
 
     log_output(settings_str, logger=logging)
 
     log_output("Beginning analysis...", logger=logging)
 
-    if not os.path.isdir(settings.output_dir):
+    # If analysis output directory exists, verify that it's either empty, or that overwrite is allowed.
+    # Otherwise create directory.
+    if os.path.isdir(settings.output_dir):
+
+        analysis_files = glob(os.path.join(settings.output_dir, '*'))
+
+        if analysis_files:
+            if not settings.overwrite:
+                raise DuplicateFile("The output directory is not empty, and overwrite is set to False. Aborting...")
+            else:
+                rm_files = glob(os.path.join(settings.output_dir, '*'))
+                list(map(shutil.rmtree, rm_files))
+    else:
         create_path(settings.output_dir)
 
     # Create summary file and add the header row
